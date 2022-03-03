8cfc071fba14b00f9c07b1d89aabf9b3cdfdfb2c
Amine Ouadrani <58174888+Aminouad@users.noreply.github.com>
2022-03-03 17:40:10 +0100

    add the controllers and the database relation

---
+++ EmployeeManager/Program.cs
@@ -0,0 +1,41 @@
+using Newtonsoft.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+}
+
+
+);
+builder.Services.AddControllers();
+
+//JSON Serializer
+builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
+options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+    .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
+    = new DefaultContractResolver());
+
+
+
+var app = builder.Build();
+
+// Enable CORS
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+ 
+
+
+
+// Configure the HTTP request pipeline.
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
